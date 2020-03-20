a4645a65ffb018e912ccc57d3eb8c9877f4bfafc
jzl <958136981@qq.com>
2020-03-20 10:48:53 +0800

    添加模块

---
+++ micro-log/src/main/java/com/micro/microlog/MicroLogApplication.java
@@ -0,0 +1,13 @@
+package com.micro.microlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroLogApplication.class, args);
+    }
+
+}
